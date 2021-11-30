b3963642334b0399bbae97f9dc70ad952d222ab6
Roger Brasco <brascoroger@gmail.com>
2021-12-01 00:35:44 +0100

    Sh and sh

--- scripts/input_tree_generator.py
+++ scripts/input_tree_generator.py
@@ -65,28 +65,34 @@ class uTree(Tree):
         self.trunk = name
         self.branches = {}
         self.node = None
+    def __eq__(self, other):
+        # breakpoint()
+        return bool(not other.node)
 
 
-operad = Operad()
-operads = []
+# operad = Operad()
+# operads = []
 
-tree_str = "o0(c2,c1;c0)|o1(c3,c4;c1)|o2(c5,c6;c2)"
-operations = tree_str.split("|")
-regex = r"(o.*)\((.*)\)"
-for operation in operations:
-    match = re.match(regex, operation)
-    if match:
-        operation, parameters = match.groups()
-        branches, trunk = parameters.split(";")
-        operads.append(Tree(operation, trunk, branches.split(","), operad))
+# tree_str = "o0(c2,c1;c0)|o1(c3,c4;c1)|o2(c5,c6;c2)"
+# operations = tree_str.split("|")
+# regex = r"(o.*)\((.*)\)"
+# for operation in operations:
+#     match = re.match(regex, operation)
+#     if match:
+#         operation, parameters = match.groups()
+#         branches, trunk = parameters.split(";")
+#         operads.append(Tree(operation, trunk, branches.split(","), operad))
 
-print(operad.colors)
-print(operad.operations)
-t = operads[0]
-# breakpoint()
-print(t.print_edges())
+# print(operad.colors)
+# print(operad.operations)
+# t = operads[0]
+# # breakpoint()
+# print(t.print_edges())
 
 
 
 # t = Tree.unitary_tree("hola")
 # breakpoint()
+
+t = uTree("hola")
+breakpoint()
