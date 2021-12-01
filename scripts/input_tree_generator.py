fb40c541343114b72b56b2b83c7029de2d78b750
Roger Brasco <brascoroger@gmail.com>
2021-12-01 13:32:13 +0100

    Session

--- scripts/input_tree_generator.py
+++ scripts/input_tree_generator.py
@@ -1,24 +1,10 @@
-from collections import defaultdict
-import re
-
-
 class Operad:
     def __init__(self):
         self.colors = {}
         self.operations = {}
 
     def add_color(self, name, prop={}):
-        if name in self.colors.keys():
-            t = self.colors[name]
-            self.colors[name] = (t, prop)
-        else:
-            self.colors[name] = prop
-        return name
-
-    def add_color_for_trunk(self, name, prop={}):
-        t = None
-        if name in self.colors.keys():
-            t = self.colors[name]
+        if t:=self.colors.get(name):
             self.colors[name] = (t, prop)
         else:
             self.colors[name] = prop
@@ -32,12 +18,12 @@ class Operad:
 class Tree:
     def __init__(self, operation, trunk, branches, operad):
 
-        trunk, parent = operad.add_color_for_trunk(trunk, self)
+        trunk, parent = operad.add_color(trunk, self)
         self.trunk = trunk
         if parent:
             parent.branches[trunk] = self
         self.branches = {
-            operad.add_color(branch, self): uTree(branch) for branch in branches
+            operad.add_color(branch, self)[0]: uTree(branch) for branch in branches
         }
         self.node = operad.add_operation(operation, self)
 
@@ -59,40 +45,87 @@ class Tree:
             ]
         )
 
+    def __str__(self):
+        return f"{self.node}({','.join(self.branches.keys())};{self.trunk})"
+
 
 class uTree(Tree):
     def __init__(self, name):
         self.trunk = name
         self.branches = {}
         self.node = None
-    def __eq__(self, other):
-        # breakpoint()
-        return bool(not other.node)
+
+import re
+
+def string_to_tree_space(string, operad):
+
+    operads = []
+    operations = string.split("|")
+    regex = r"(.*)\((.*)\)"
+    for operation in operations:
+        match = re.match(regex, operation)
+        if not match:
+            raise RuntimeError(f"Operation not defined correctly {operation}")
+        operation, parameters = match.groups()
+        branches, trunk = parameters.split(";")
+        operads.append(Tree(operation, trunk, branches.split(","), operad))
+    return operads[0], operad
 
 
-# operad = Operad()
-# operads = []
-
-# tree_str = "o0(c2,c1;c0)|o1(c3,c4;c1)|o2(c5,c6;c2)"
-# operations = tree_str.split("|")
-# regex = r"(o.*)\((.*)\)"
-# for operation in operations:
-#     match = re.match(regex, operation)
-#     if match:
-#         operation, parameters = match.groups()
-#         branches, trunk = parameters.split(";")
-#         operads.append(Tree(operation, trunk, branches.split(","), operad))
-
-# print(operad.colors)
-# print(operad.operations)
-# t = operads[0]
-# # breakpoint()
-# print(t.print_edges())
+def tree_space_to_string(tree_space):
+    _, operad = tree_space
+    return "|".join(str(tree) for tree in operad.operations.values())
 
 
+# S = string_to_tree_space("o0w(c2,c1;c0)", Operad())
+# T = string_to_tree_space("o0b(d2,d1;d0)", Operad())
+# T = string_to_tree_space("o0w(c2,c1;c0)|o1w(c3,c4;c1)|o2w(c5,c6;c2)", Operad())
+# S = string_to_tree_space("o0b(d2,d1;d0)|o1b(d3,d4;d1)|o2b(d5,d6;d2)", Operad())
+# S = string_to_tree_space("W(b,c;a)", Operad())
+# T = string_to_tree_space("B(1;0)", Operad())
+# S = string_to_tree_space("0W(1;0)|1W(2,3;1)", Operad())
+# T = string_to_tree_space("0B(b,d;a)|1B(c;b)|2B(e;d)", Operad())
+# S = string_to_tree_space("0W(1;0)|1W(2,4,6;1)|2W(3;2)|3W(5;4)|4W(7;6)", Operad())
+# T = string_to_tree_space("0B(b,f;a)|1B(c;b)|2B(d;c)|3B(e;d)|4B(;f)", Operad())
+S = string_to_tree_space("0W(1;0)|1W(2,3;1)|2W(;3)", Operad())
+T = string_to_tree_space("0B(b,c;a)|1B(;c)", Operad())
+print(tree_space_to_string(S))
+print(tree_space_to_string(T))
 
-# t = Tree.unitary_tree("hola")
-# breakpoint()
 
-t = uTree("hola")
-breakpoint()
+import math
+def Sh(S, T):
+    if isinstance(S, uTree) and isinstance(T, uTree):
+        return str(S), str(T)
+    if isinstance(S, uTree):
+        return str(S), [Sh(S, Ti) for Ti in T.branches.values()]
+    if isinstance(T, uTree):
+        return [Sh(Si, T) for Si in S.branches.values()], str(T)
+
+    return [Sh(Si, T) for Si in S.branches.values()], [
+        Sh(S, Ti) for Ti in T.branches.values()
+    ]
+
+# Sin simetria
+def sh(S, T):
+    if isinstance(S, uTree):
+        return 1 
+    if isinstance(T, uTree):
+        return  1
+    breakpoint()
+    return prod([sh(Si, T) for Si in S.branches.values()]) + prod([
+        sh(S, Ti) for Ti in T.branches.values()
+    ])
+    
+def prod(array):
+    if not len(array):
+        print("hey")
+        return 0
+    x = 1
+    for e in array:
+        x*=e
+    return x
+    
+Sh_lattice = Sh(S[0], T[0])
+sh_number = sh(S[0], T[0])
+print(sh_number)
